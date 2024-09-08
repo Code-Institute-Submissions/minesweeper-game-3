1aec64fb013f338a2bea3e5eb81392ecc4bf2f70
Norbert Tiborcz <norbert.tiborcz@gmail.com>
2024-09-08 17:32:40 +0000

    Redefine theme color values

--- configurations.py
+++ configurations.py
@@ -4,7 +4,7 @@ from enum import Enum
 class Hue(Enum):
     ORANGE = 33
     YELLOW = 50
-    GREEN = 100
+    GREEN = 110
     BLUE = 200
     PURPLE = 266
     PINK = 320
@@ -12,17 +12,17 @@ class Hue(Enum):
 
 
 class LightTheme(Enum):
-    PRIMARY_BACKGROUND = (1, 0.95)
+    PRIMARY_BACKGROUND = (0.5, 0.95)
     SECONDARY_BACKGROUND = (0.75, 0.85)
-    PRIMARY_ACCENT = (1, 0.5)
-    SECONDARY_ACCENT = (1, 0.35)
+    PRIMARY_ACCENT = (1, 0.45)
+    SECONDARY_ACCENT = (1, 0.25)
     FONT_COLOR = (0.15, 0.15)
 
 
 class DarkTheme(Enum):
     PRIMARY_BACKGROUND = (0.16, 0.16)
     SECONDARY_BACKGROUND = (0.15, 0.25)
-    PRIMARY_ACCENT = (1, 0.6)
+    PRIMARY_ACCENT = (0.9, 0.6)
     SECONDARY_ACCENT = (1, 0.8)
     FONT_COLOR = (0.95, 0.95)
 
